18ea475455ab8ea651dc28c73d07003c39e5ebc2
Maciej Zubala <maciej.zubala@gmail.com>
2017-11-20 07:12:40 +0100

    Revert "Zad 1 user"
    This reverts commit 4320d6f1c11e3c08b855fd3ce63ec56cb7a856fb.

--- user/src/main/java/pl/com/bottega/microecom/user/infrastructure/SpringSecurityConfig.java
+++ user/src/main/java/pl/com/bottega/microecom/user/infrastructure/SpringSecurityConfig.java
@@ -20,12 +20,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().
-                addFilterBefore(new ApiAuthFilter(authenticationRepository), UsernamePasswordAuthenticationFilter.class).
-                authorizeRequests().
-                    antMatchers("/registrations", "/authentications").permitAll().
-                    antMatchers("/users/**", "PUT").hasRole("ADMIN").
-                anyRequest().authenticated();
+        // TODO Zadanie 1
     }
 
 
